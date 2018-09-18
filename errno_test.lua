bb041446453ba28637fb3d4fb6c8003944c7e4ca
Ruzsa Balazs <cellux@omkamra.hu>
2018-09-18 21:37:12 +0200

    test: calling errno.errno() does not consume the error code

--- errno_test.lua
+++ errno_test.lua
@@ -8,5 +8,7 @@ testing("errno", function()
    local res = ffi.C.open("/xxx/non-existent", ffi.C.O_RDONLY, 0)
    assert(res==-1)
    assert(errno.errno()==ffi.C.ENOENT)
+   --  calling errno.errno() does not consume the error code
+   assert(errno.errno()==ffi.C.ENOENT)
    assert.equals(errno.strerror(), "No such file or directory")
 end)
