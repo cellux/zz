d09574734ec081cd07fe433d7963395e390b2165
rb <Ruzsa Balazs ruzsa.balazs@gmail.com>
2018-11-05 21:57:04 +0100

    fix util.round() to work on 64-bit architectures

--- util.lua
+++ util.lua
@@ -7,9 +7,9 @@ local M = {}
 
 function M.round(x)
    if x >= 0 then
-      return math.floor(x+0.5)
+      return math.floor(tonumber(x) + 0.5)
    else
-      return math.ceil(x-0.5)
+      return math.ceil(tonumber(x) - 0.5)
    end
 end
 
